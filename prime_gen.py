581037221195b2abff39cd9989624e936dfd8cd1
V. Hanuman Kumar <vhanumankumar@gmail.com>
2024-09-12 15:28:36 +0530

    Add files via upload

---
+++ prime_gen.py
@@ -0,0 +1,21 @@
+def prime_chk(n):
+    c = 0
+    for i in range(2,n):
+        if n % i == 0:
+            c = c + 1
+    if c == 0:
+        return 1
+    else:
+        return 0
+
+
+LB = int(input("Enter LB:"))
+UB = int(input("Enter UB:"))
+
+for val in range(LB, UB+1):
+    if prime_chk(val) == 1:
+        print(val)
+        
+         
+
+
